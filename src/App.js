a79c2aceb4dc99bf4e64be8337e1e57d67dca824
laurabrovkina <laurabrovkina@mail.ru>
2018-03-09 21:09:33 +1300

    add new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "ed25f33c0b2558e07c08f99d003da857";
 
 class App extends React.Component{
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return(
-      <div>Hello</div>
+      <div>
+      <Titles />
+      <Form getWeather={this.getWeather}/>
+      <Weather />
+      </div>
     );
   }
 };
